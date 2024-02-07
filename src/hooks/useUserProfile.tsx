4a80aa62a1fca03c6f163e210df8b673039b6b6b
AdrianF3 <adrianf.webdev@gmail.com>
2024-02-06 18:25:48 -0700

    Version 0.1.1

--- src/hooks/useUserProfile.tsx
+++ src/hooks/useUserProfile.tsx
@@ -1,41 +1,48 @@
 import { useState, useEffect } from 'react';
+import { doc, getDoc, collection } from 'firebase/firestore';
+import { db } from '../firebaseConfig'
 import { UserProfile } from '../types/UserProfile.types';
 
-// Hook to get user profile data
-export default function useUserProfile() {
-
-  // State to hold the profile data
+const useUserProfile = (uid: string | null) => {
   const [profile, setProfile] = useState<UserProfile | null>(null);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
-    // Mock async data fetching
-    const fetchProfile = async () => {
-      try {
-        // Simulate fetching data with a delay
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-        
-        // Hardcoded data to simulate a user profile
-        const userProfile: UserProfile = {
-          id: 'user-123',
-          name: 'John Doe',
-          email: 'john.doe@example.com',
-          bio: 'Software Developer with a passion for web technologies.',
-          theme: 'nord',
-          zipCode: '80112'
-        };
+    console.log('uid', uid)
+    if (uid) {
+      console.log('called')
+      
+      const fetchProfile = async () => {
+        try {
+          console.log('fetch profile called')
+          setLoading(true);
+          const docRef = doc(db, 'userProfile', uid); // Adjusted for Firebase Modular SDK
+          const docSnap = await getDoc(docRef);
+          console.log('docSnap', docSnap)
 
-        setProfile(userProfile);
-        setLoading(false);
-      } catch (e) {
-        setError(e instanceof Error ? e : new Error('An unknown error occurred'));
-        setLoading(false);
-      }
-    };
+          if (docSnap.exists()) {
+            setProfile(docSnap.data() as UserProfile);
+          } else {
+            setProfile(null);
+            setError(new Error('No profile found'));
+          }
+        } catch (e) {
+          setError(e instanceof Error ? e : new Error('An unknown error occurred'));
+        } finally {
+          setLoading(false);
+        }
+      };
 
-    fetchProfile();
-  }, []);
+      fetchProfile();
+    } else {
+      console.log('else called')
+      setLoading(false);
+      setProfile(null);
+    }
+  }, [uid]);
 
   return { profile, loading, error };
-}
+};
+
+export default useUserProfile;
